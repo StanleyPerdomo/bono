be76f71d07c9a4f62173b56d969b3daa1486fbad
StanleyPerdomo <pd07020@ues.edu.sv>
2024-09-12 22:08:29 -0600

    Add project files.

---
+++ bono/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace bono
+{
+    partial class CalculoDeBono
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbSueldo = new System.Windows.Forms.TextBox();
+            this.gbCategoria = new System.Windows.Forms.GroupBox();
+            this.rbGerencia = new System.Windows.Forms.RadioButton();
+            this.rbVentas = new System.Windows.Forms.RadioButton();
+            this.rbInformatica = new System.Windows.Forms.RadioButton();
+            this.rbAdministrativo = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBono = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.gbCategoria.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(51, 84);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(51, 133);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(40, 13);
+            this.lblSueldo.TabIndex = 1;
+            this.lblSueldo.Text = "Sueldo";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(131, 80);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbSueldo
+            // 
+            this.tbSueldo.Location = new System.Drawing.Point(131, 129);
+            this.tbSueldo.Name = "tbSueldo";
+            this.tbSueldo.Size = new System.Drawing.Size(100, 20);
+            this.tbSueldo.TabIndex = 3;
+            // 
+            // gbCategoria
+            // 
+            this.gbCategoria.Controls.Add(this.rbAdministrativo);
+            this.gbCategoria.Controls.Add(this.rbInformatica);
+            this.gbCategoria.Controls.Add(this.rbVentas);
+            this.gbCategoria.Controls.Add(this.rbGerencia);
+            this.gbCategoria.Location = new System.Drawing.Point(275, 46);
+            this.gbCategoria.Name = "gbCategoria";
+            this.gbCategoria.Size = new System.Drawing.Size(200, 151);
+            this.gbCategoria.TabIndex = 4;
+            this.gbCategoria.TabStop = false;
+            this.gbCategoria.Text = "Categoria";
+            // 
+            // rbGerencia
+            // 
+            this.rbGerencia.AutoSize = true;
+            this.rbGerencia.Checked = true;
+            this.rbGerencia.Location = new System.Drawing.Point(0, 20);
+            this.rbGerencia.Name = "rbGerencia";
+            this.rbGerencia.Size = new System.Drawing.Size(68, 17);
+            this.rbGerencia.TabIndex = 0;
+            this.rbGerencia.TabStop = true;
+            this.rbGerencia.Text = "Gerencia";
+            this.rbGerencia.UseVisualStyleBackColor = true;
+            // 
+            // rbVentas
+            // 
+            this.rbVentas.AutoSize = true;
+            this.rbVentas.Location = new System.Drawing.Point(0, 52);
+            this.rbVentas.Name = "rbVentas";
+            this.rbVentas.Size = new System.Drawing.Size(58, 17);
+            this.rbVentas.TabIndex = 1;
+            this.rbVentas.Text = "Ventas";
+            this.rbVentas.UseVisualStyleBackColor = true;
+            // 
+            // rbInformatica
+            // 
+            this.rbInformatica.AutoSize = true;
+            this.rbInformatica.Location = new System.Drawing.Point(0, 84);
+            this.rbInformatica.Name = "rbInformatica";
+            this.rbInformatica.Size = new System.Drawing.Size(77, 17);
+            this.rbInformatica.TabIndex = 2;
+            this.rbInformatica.Text = "Informatica";
+            this.rbInformatica.UseVisualStyleBackColor = true;
+            // 
+            // rbAdministrativo
+            // 
+            this.rbAdministrativo.AutoSize = true;
+            this.rbAdministrativo.Location = new System.Drawing.Point(0, 116);
+            this.rbAdministrativo.Name = "rbAdministrativo";
+            this.rbAdministrativo.Size = new System.Drawing.Size(90, 17);
+            this.rbAdministrativo.TabIndex = 3;
+            this.rbAdministrativo.Text = "Administrativo";
+            this.rbAdministrativo.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bonificacion";
+            // 
+            // lblBono
+            // 
+            this.lblBono.AutoSize = true;
+            this.lblBono.Location = new System.Drawing.Point(156, 225);
+            this.lblBono.Name = "lblBono";
+            this.lblBono.Size = new System.Drawing.Size(0, 13);
+            this.lblBono.TabIndex = 6;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(90, 327);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(267, 327);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Añadir categoria";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Items.AddRange(new object[] {
+            "Categoria1",
+            "Categoria 2"});
+            this.cbCategoria.Location = new System.Drawing.Point(275, 278);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cbCategoria.TabIndex = 10;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(159, 245);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbCategoria.TabIndex = 11;
+            // 
+            // CalculoDeBono
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 450);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblBono);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gbCategoria);
+            this.Controls.Add(this.tbSueldo);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lblSueldo);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "CalculoDeBono";
+            this.Text = "Calculo de Bono";
+            this.Load += new System.EventHandler(this.CalculoDeBono_Load);
+            this.gbCategoria.ResumeLayout(false);
+            this.gbCategoria.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbSueldo;
+        private System.Windows.Forms.GroupBox gbCategoria;
+        private System.Windows.Forms.RadioButton rbAdministrativo;
+        private System.Windows.Forms.RadioButton rbInformatica;
+        private System.Windows.Forms.RadioButton rbVentas;
+        private System.Windows.Forms.RadioButton rbGerencia;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBono;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.TextBox tbCategoria;
+    }
+}
+
---
+++ bono/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bono
+{
+    public partial class CalculoDeBono : Form
+    {
+        public CalculoDeBono()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculoDeBono_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double bono = 0;
+            
+            if(rbGerencia.Checked)
+            {
+                bono = 500;
+            }
+
+            if (rbVentas.Checked)
+            {
+                bono = 300;
+            }
+
+            if (rbInformatica.Checked)
+            {
+                bono = 200;
+            }
+
+            if (rbAdministrativo.Checked)
+            {
+                bono = 100;
+            }
+
+            lblBono.Text = bono.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double bono = 0;
+            int seleccion = cbCategoria.SelectedIndex;
+
+            switch(seleccion)
+            {
+                case 0:
+                    bono = 500;
+                    break;
+                case 1:
+                    bono = 300;
+                    break;
+
+                case 2:
+                    bono = 200;
+                    break;
+                default:
+                    bono = 0;
+                    break;
+
+            }
+            //lblBono.Text = bono.ToString();
+            lblBono.Text = cbCategoria.SelectedIndex.ToString();
+                }
+    }
+}
